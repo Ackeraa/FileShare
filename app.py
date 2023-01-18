ccc0fbbbddcb08e75a39ed3ec839612088f8e0b5
Ackeraa <acker2880@gmail.com>
2023-01-19 00:17:44 +0800

    finished first version

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+from flask import Flask, send_file, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+app = Flask(__name__)
+
+limiter = Limiter(key_func=get_remote_address, app=app)
+
+@app.route("/download/<filename>")
+@limiter.limit("1 per 3 minute")
+def download(filename):
+    # if share folder not exist, create it
+    if not os.path.exists("share"):
+        os.makedirs("share")
+
+    file_path = os.path.join("share", filename)
+    if os.path.exists(file_path):
+        shutil.make_archive(file_path, 'zip', file_path)
+
+        res = send_file(file_path + ".zip", as_attachment=True)
+
+        os.remove(file_path + ".zip")
+
+        return res
+    else:
+        return jsonify(error="file does not exists"), 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False)
